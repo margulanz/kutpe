35ecc0e2d12a24a99b4460d9963b0661010a5015
nurbekakbolat9 <nurbekakbolat9@gmail.com>
2024-04-19 16:08:24 +0500

    gpt integrated

--- users/utils.py
+++ users/utils.py
@@ -6,27 +6,58 @@ import joblib
 import numpy as np
 from django.conf import settings
 import datetime
+import google.generativeai as genai
+import os
+
+API_KEY = "AIzaSyBO4vStpGCCBkxFDTiUy9zPOFdXkmK_cjc"
+
+
+def generate_response():
+    genai.configure(api_key=API_KEY)
+    model = genai.GenerativeModel("gemini-pro")
+    time = datetime.datetime.now()
+    minute = time.minute
+    rounded_minutes = minute // 10 * 10
+    hour = time.hour
+
+    original_time = f"{hour:02d}:{time.minute:02d}"
+    rounded_time = f"{hour:02d}:{rounded_minutes:02d}"
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    input_file_path = os.path.join(script_dir, "input.txt")
+
+    with open(input_file_path, "r") as file:
+        text = file.read()
+
+    text = (
+        text
+        + f"Given the dataset's information about arrivals in 10-minute intervals, the current time is {original_time}, corresponding to the data point closest to {rounded_time}. Please advise whether visiting the place is recommended based on its arrival rate at this time. Keep the response short and informative, focusing on whether it's overcrowded or not. For example, you might say, 'It's overcrowded right now,' or 'It's not too crowded at the moment.'"
+    )
+
+    response = model.generate_content(text)
+
+    return response.text
 
 
 def generate_otp(length=6):
     characters = string.digits
-    otp = ''.join(random.choice(characters) for _ in range(length))
+    otp = "".join(random.choice(characters) for _ in range(length))
     return otp
 
 
 def send_sms_otp(phone_number, otp):
     # Mobizon API endpoint and API key
     api_key = "some_key"  # os.environ.get('API_KEY')
-    api_url = f'https://api.mobizon.kz/service/Message/SendSmsMessage?apiKey={api_key}'
+    api_url = f"https://api.mobizon.kz/service/Message/SendSmsMessage?apiKey={api_key}"
 
     # SMS message parameters
     recipient = str(phone_number)[1:]
-    text = f'Код для активации аккаунта: {otp}'
+    text = f"Код для активации аккаунта: {otp}"
 
     # Construct the request payload
     payload = {
-        'recipient': recipient,
-        'text': text,
+        "recipient": recipient,
+        "text": text,
     }
 
     # Make the API request
@@ -37,10 +68,7 @@ def send_sms_otp(phone_number, otp):
 
 def get_location_coordinates(location_str):
     nominatim_url = "https://nominatim.openstreetmap.org/search"
-    params = {
-        'format': 'json',
-        'q': location_str
-    }
+    params = {"format": "json", "q": location_str}
 
     response = requests.get(nominatim_url, params=params)
     data = response.json()
@@ -52,7 +80,7 @@ def get_location_coordinates(location_str):
         return None
 
 
-def find_nearest_banks(location, radius=1500, amenity='bank'):
+def find_nearest_banks(location, radius=1500, amenity="bank"):
     # location = get_location_coordinates(location_str)
 
     if location:
@@ -64,14 +92,14 @@ def find_nearest_banks(location, radius=1500, amenity='bank'):
         """
 
         response = requests.post(overpass_url, data=overpass_query)
-        data = response.json().get('elements', [])
+        data = response.json().get("elements", [])
 
         return data
     else:
         print("Invalid location.")
 
 
-def find_nearest_banks_by_location(location_str, radius=1500, amenity='bank'):
+def find_nearest_banks_by_location(location_str, radius=1500, amenity="bank"):
     location = get_location_coordinates(location_str)
 
     if location:
@@ -83,17 +111,14 @@ def find_nearest_banks_by_location(location_str, radius=1500, amenity='bank'):
         """
 
         response = requests.post(overpass_url, data=overpass_query)
-        data = response.json().get('elements', [])
+        data = response.json().get("elements", [])
 
         return data
     else:
         print("Invalid location.")
 
 
-WeekDay_business = {
-    'Monday': [30, 10, 7],
-    'Tuesday': [30, 10, 7]
-}
+WeekDay_business = {"Monday": [30, 10, 7], "Tuesday": [30, 10, 7]}
 
 
 def datetime_to_features(dt):
@@ -109,7 +134,7 @@ def datetime_to_features(dt):
 def arrival_interval_time(time):
     # now = datetime.datetime.now()
     data = datetime_to_features(time)
-    model = joblib.load('backend/finalized_model.pkl')
+    model = joblib.load("backend/finalized_model.pkl")
     features = np.array([data])
     print(features, flush=True)
     predicted_arrivals = model.predict(features)
@@ -118,19 +143,22 @@ def arrival_interval_time(time):
 
 def calculate_time(time, num_servers=2, max_service=20, min_service=10):
     # now = datetime.datetime.now()
-    lambdaa = 1/arrival_interval_time(time)
-    mean_service_time = (min_service + max_service)/2
-    mewing = 1/mean_service_time
-    variance_s = pow(max_service-min_service, 2)/12
-    p = lambdaa/(num_servers*mewing)
-    C_s = variance_s/pow(mean_service_time, 2)
+    lambdaa = 1 / arrival_interval_time(time)
+    mean_service_time = (min_service + max_service) / 2
+    mewing = 1 / mean_service_time
+    variance_s = pow(max_service - min_service, 2) / 12
+    p = lambdaa / (num_servers * mewing)
+    C_s = variance_s / pow(mean_service_time, 2)
     sum = 0
-    for i in range(num_servers-1):
-        sum += pow(num_servers*p, i)/math.factorial(i)
-    sum += pow(num_servers*p, num_servers) / \
-        (math.factorial(num_servers)*(1 - p))
-    p_0 = 1/sum
-    L_q = p_0*pow(lambdaa/mewing, num_servers)*p / \
-        (math.factorial(num_servers)*pow(1-p, 2))
-    W_q = L_q/lambdaa
+    for i in range(num_servers - 1):
+        sum += pow(num_servers * p, i) / math.factorial(i)
+    sum += pow(num_servers * p, num_servers) / (math.factorial(num_servers) * (1 - p))
+    p_0 = 1 / sum
+    L_q = (
+        p_0
+        * pow(lambdaa / mewing, num_servers)
+        * p
+        / (math.factorial(num_servers) * pow(1 - p, 2))
+    )
+    W_q = L_q / lambdaa
     return (L_q, W_q)
--- users/views.py
+++ users/views.py
@@ -8,17 +8,25 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .utils import find_nearest_banks, find_nearest_banks_by_location, calculate_time
+from .utils import (
+    find_nearest_banks,
+    find_nearest_banks_by_location,
+    calculate_time,
+    generate_response,
+)
 from .models import Queue, Participant, User, Organization
 from .serializers import PhoneOTPSerializer, QueueSerializer, UserSerializer
+
 # Create your views here.
 
 
 class CustomRegisterView(RegisterView):
     def perform_create(self, serializer):
         user = serializer.save(self.request)
-        if allauth_account_settings.EMAIL_VERIFICATION != \
-                allauth_account_settings.EmailVerificationMethod.MANDATORY:
+        if (
+            allauth_account_settings.EMAIL_VERIFICATION
+            != allauth_account_settings.EmailVerificationMethod.MANDATORY
+        ):
             if api_settings.USE_JWT:
                 self.access_token, self.refresh_token = jwt_encode(user)
             elif not api_settings.SESSION_LOGIN:
@@ -37,25 +45,31 @@ class VerifyPhone(APIView):
             print(validated_data)
             try:
                 phone_otp = PhoneOTP.objects.get(
-                    phone_number=validated_data['phone_number'])
+                    phone_number=validated_data["phone_number"]
+                )
             except:
-                return Response("No such phone number", status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    "No such phone number", status=status.HTTP_404_NOT_FOUND
+                )
 
-            otp = validated_data['otp']
+            otp = validated_data["otp"]
             current_time = timezone.now()
             if current_time < phone_otp.active_until and phone_otp.otp == otp:
                 user = User.objects.get(phone_number=phone_otp.phone_number)
                 user.is_active = True
                 user.save()
                 phone_otp.delete()
-                return Response(f"phone number {phone_otp.phone_number} is verified", status=status.HTTP_200_OK)
+                return Response(
+                    f"phone number {phone_otp.phone_number} is verified",
+                    status=status.HTTP_200_OK,
+                )
 
         return Response("Data is invalid", status=status.HTTP_400_BAD_REQUEST)
 
 
 class SendOTP(APIView):
     def post(self, request, *args, **kwargs):
-        id_ = self.kwargs.get('id', None)
+        id_ = self.kwargs.get("id", None)
         if id_ and User.objects.filter(id=id_).exists():
             phone_number = User.objects.get(id=id_).phone_number
             create_and_send_otp(phone_number)
@@ -63,81 +77,100 @@ class SendOTP(APIView):
         return Response("Data is invalid", status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(('POST',))
+@api_view(("POST",))
 def add_to_queue(request, org_id, user_id):
     try:
-        service_name = request.data.get('service_name')
-        queue = Queue.objects.get(
-            org__org_id=org_id, service_name=service_name)
+        service_name = request.data.get("service_name")
+        queue = Queue.objects.get(org__org_id=org_id, service_name=service_name)
         user = User.objects.get(id=user_id)
     except:
-        return Response(f"Something wrong with request", status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            f"Something wrong with request", status=status.HTTP_400_BAD_REQUEST
+        )
     now = datetime.datetime.now()
     participant = Participant.objects.create(
-        user=user, position=queue.count+1, waiting_time=calculate_time(now)[1])
+        user=user, position=queue.count + 1, waiting_time=calculate_time(now)[1]
+    )
     # check if empty
     if queue.count == 0:
         queue.current_pos = participant.position
     queue.count += 1
     queue.save()
     queue.participants.add(participant)
-    return Response(f"{user} is added to queue with position {participant.position}", status=status.HTTP_200_OK)
+    return Response(
+        f"{user} is added to queue with position {participant.position}",
+        status=status.HTTP_200_OK,
+    )
 
 
-@api_view(('POST',))
+@api_view(("POST",))
 def process_current_pos(request, org_id):
     try:
-        service_name = request.data.get('service_name')
-        queue = Queue.objects.get(
-            org__org_id=org_id, service_name=service_name)
+        service_name = request.data.get("service_name")
+        queue = Queue.objects.get(org__org_id=org_id, service_name=service_name)
         participant = Participant.objects.get(position=queue.current_pos)
     except:
-        return Response(f"Something wrong with request", status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            f"Something wrong with request", status=status.HTTP_400_BAD_REQUEST
+        )
     if queue.participants.count():
         queue.current_pos += 1
         queue.save()
         queue.participants.remove(participant)
         participant.delete()
-        return Response(f"{participant} was processed and current queue pos is  {queue.current_pos}", status=status.HTTP_200_OK)
+        return Response(
+            f"{participant} was processed and current queue pos is  {queue.current_pos}",
+            status=status.HTTP_200_OK,
+        )
 
 
 class GetNearestBanks(APIView):
     def post(self, request, **kwargs):
 
-        lat = request.data.get('lat')
-        lon = request.data.get('lon')
+        lat = request.data.get("lat")
+        lon = request.data.get("lon")
         data = find_nearest_banks(f"{lat},{lon}")
         return Response(data, status=status.HTTP_200_OK)
 
 
 class GetNearestBanksByLocation(APIView):
     def post(self, request, **kwargs):
-        location = request.data.get('location')
+        data = generate_response()
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class GetGPTAdvice(APIView):
+    def post(self, request, **kwargs):
+        location = request.data.get("location")
         data = find_nearest_banks_by_location(location)
         return Response(data, status=status.HTTP_200_OK)
 
 
 class GetTime(APIView):
     def post(self, request, **kwargs):
-        num_servers = request.data.get('num_servers')
+        num_servers = request.data.get("num_servers")
         # inter_arrival_time = request.data.get('inter_arrival_time')
-        max_service = request.data.get('max_service')
-        min_service = request.data.get('min_service')
+        max_service = request.data.get("max_service")
+        min_service = request.data.get("min_service")
         now = datetime.datetime.now()
-        average_l, mean_t = calculate_time(now,
-                                           num_servers, max_service, min_service)
-        return Response({"average_l": average_l, "mean_t": mean_t}, status=status.HTTP_200_OK)
+        average_l, mean_t = calculate_time(now, num_servers, max_service, min_service)
+        return Response(
+            {"average_l": average_l, "mean_t": mean_t}, status=status.HTTP_200_OK
+        )
 
 
 class GetWaitingTime(APIView):
     def post(self, request, org_id, **kwargs):
-        service_name = request.data.get('service_name')
+        service_name = request.data.get("service_name")
         queue = Queue.objects.filter(
-            org__org_id=org_id, service_name=service_name).first()
+            org__org_id=org_id, service_name=service_name
+        ).first()
         if queue:
             now = datetime.datetime.now()
             data = {
-                "waiting_time": calculate_time(now, queue.num_servers, queue.max_service, queue.min_service)[1]
+                "waiting_time": calculate_time(
+                    now, queue.num_servers, queue.max_service, queue.min_service
+                )[1]
             }
             return Response(data, status=status.HTTP_200_OK)
         return Response("There is no such queue", status=status.HTTP_200_OK)
@@ -145,9 +178,10 @@ class GetWaitingTime(APIView):
 
 class GetQueue(APIView):
     def post(self, request, org_id, **kwargs):
-        service_name = request.data.get('service_name')
+        service_name = request.data.get("service_name")
         queue = Queue.objects.filter(
-            org__org_id=org_id, service_name=service_name).first()
+            org__org_id=org_id, service_name=service_name
+        ).first()
         if queue:
             serializer = QueueSerializer(queue)
 
@@ -156,27 +190,33 @@ class GetQueue(APIView):
 
 class RemoveFromQueue(APIView):
     def post(self, request, user_id, org_id, **kwargs):
-        service_name = request.data.get('service_name')
+        service_name = request.data.get("service_name")
         queue = Queue.objects.filter(
-            org__org_id=org_id, service_name=service_name).first()
+            org__org_id=org_id, service_name=service_name
+        ).first()
         participant = Participant.objects.filter(user__id=user_id).first()
-        if (queue and participant) and (queue.participants.filter(id=participant.id).exists()):
+        if (queue and participant) and (
+            queue.participants.filter(id=participant.id).exists()
+        ):
             queue.participants.remove(participant)
             participant.delete()
             return Response("Participant was removed", status=status.HTTP_200_OK)
-        return Response("There is no such queue or participant", status=status.HTTP_200_OK)
+        return Response(
+            "There is no such queue or participant", status=status.HTTP_200_OK
+        )
 
 
 class CreateUser(APIView):
     def post(self, request, **kwargs):
-        phone_number = request.data.get('phone_number')
+        phone_number = request.data.get("phone_number")
         try:
-            user = User.objects.create(
-                username=phone_number, phone_number=phone_number)
+            user = User.objects.create(username=phone_number, phone_number=phone_number)
             create_and_send_otp(phone_number, user)
             return Response("Password was sent", status=status.HTTP_200_OK)
         except:
-            return Response("Something wrong with phonenumber", status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                "Something wrong with phonenumber", status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class GetUser(APIView):
@@ -190,8 +230,8 @@ class GetUser(APIView):
 
 class CreateQueue(APIView):
     def post(self, request, **kwargs):
-        org_id = request.data.get('org_id')
-        service_name = request.data.get('service_name')
+        org_id = request.data.get("org_id")
+        service_name = request.data.get("service_name")
         org = Organization.objects.filter(org_id=org_id).first()
         if not Queue.objects.filter(service_name=service_name).exists() and org:
             queue = Queue.objects.create(org=org, service_name=service_name)
